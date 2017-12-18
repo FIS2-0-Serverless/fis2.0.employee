31d518272373a74957fcb6d60239a15eda2a07c9
David Schmitz <david.schmitz.privat@gmail.com>
2017-12-18 13:41:03 +0100

    Working minimal deployment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  getAll(event, context, callback) {
+    callback(null, 'all is well')
+  },
+  getById(event, context, callback) {
+    callback(null, 'all is well')
+  }
+}
---
+++ test/index_test.js
@@ -0,0 +1,7 @@
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+
+lab.experiment('the index', () => {
+  lab.test('should do things', async () => {
+  })
+})
