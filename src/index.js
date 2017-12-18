1bef358b7bc0a1e68443689af96c54c71c26acc4
David Schmitz <david.schmitz.privat@gmail.com>
2017-12-18 14:07:38 +0100

    Basic bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const Util = require('util')
+
 module.exports = {
   getAll(event, context, callback) {
+    console.log('Event is ', Util.inspect(event))
     callback(null, 'all is well')
   },
   getById(event, context, callback) {
